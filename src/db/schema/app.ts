4337dc3c8e2527fa3007e67b9bbeda20587add98
Vlad <chepurnojv@gmail.com>
2026-01-29 07:37:40 +0200

    implemented fixed widh types in schema

--- src/db/schema/app.ts
+++ src/db/schema/app.ts
@@ -37,6 +37,6 @@ export const subjectRelations = relations(subjects, ({one, many})=> ({
 export type Department = typeof departments.$inferSelect
 export type NewDepartment = typeof departments.$inferInsert
 
-export type Subject = typeof departments.$inferSelect
-export type newSubject = typeof departments.$inferInsert
+export type Subject = typeof subjects.$inferSelect
+export type newSubject = typeof subjects.$inferInsert
 
